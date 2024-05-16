42a5f32b84abe2a33d925fe94551a891abc74c70
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-05-16 23:51:00 +0900

    feat: お問い合わせセクションの実装完了

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -193,6 +193,14 @@ export default function IndexPage() {
             <p className="text-muted-foreground sm:text-lg sm:leading-7">Post Writerはログインをするとブログ投稿ができるようになります。</p>
           </div>
       </section>
+
+      <section id="conatct" className="container py-8 md:py-12 lg:py-24">
+        <div className="max-w-[58rem] mx-auto text-center flex flex-col gap-4">
+          <h2 className="font-extrabold text-3xl md:text-6xl">Contact Me</h2>
+          <p className="text-muted-foreground sm:text-lg sm:leading-7">もしもWebサービスが気に入った場合は下記xからDMでご連絡ください。<br />お仕事のご連絡をお待ちしております。</p>
+          <Link href={siteConfig.links.x} className="underline underline-offset-4" target="_blank" rel="noreferrer">お仕事はXまで</Link>
+        </div>
+      </section>
     </>
   );
 }
