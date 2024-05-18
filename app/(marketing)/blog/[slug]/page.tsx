d8d3be82bf653aa4007584badeca8ecebea6fdf7
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-05-18 22:43:31 +0900

    feat: Dynamic Metadataで記事ごとのメタデータの設定

--- app/(marketing)/blog/[slug]/page.tsx
+++ app/(marketing)/blog/[slug]/page.tsx
@@ -1,8 +1,10 @@
 import { allPosts } from "@/.contentlayer/generated";
 import Mdx from "@/components/MdxComponent";
 import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
@@ -11,6 +13,34 @@ async function getStaticPaths(slug: string) {
   const post = allPosts.find((post) => post.slugAsParams === slug);
   return post;
 }
+
+export async function generateMetadata({params}: {params: {slug: string}}):Promise<Metadata> {
+  const page = await getStaticPaths(params.slug);
+  if(!page) {
+    return {}
+  }
+
+  return {
+    title: page.title,
+    description: page.description,
+    openGraph: {
+      type: "article",
+      locale: "ja",
+      url: siteConfig.url,
+      title: siteConfig.name,
+      description: siteConfig.description,
+      siteName: siteConfig.name,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: siteConfig.name,
+      description: siteConfig.description,
+      images: [`${siteConfig.url}/og-image.png`],
+      creator: 'ryuta'
+    }
+  }
+}
+
 export default async function PostPage({
   params,
 }: {
@@ -32,11 +62,26 @@ export default async function PostPage({
           <h1 className="mt-2 font-extrabold text-4xl lg:text-5xl leading-tight">
             {post.title}
           </h1>
-          {post.image && (<Image src={post.image} alt={post.title} width={720} height={405} className="my-8 border rounded-md bg-muted w-full" />)}
+          {post.image && (
+            <Image
+              src={post.image}
+              alt={post.title}
+              width={720}
+              height={405}
+              className="my-8 border rounded-md bg-muted w-full"
+            />
+          )}
         </div>
-        <Mdx code={post.body.code}/>
-        <hr  className="mt-12"/>
-        <div className="py-6 text-center lg:py-10"><Link href="/blog" className={cn(buttonVariants({variant: "ghost"}))}>全ての記事を見る</Link></div>
+        <Mdx code={post.body.code} />
+        <hr className="mt-12" />
+        <div className="py-6 text-center lg:py-10">
+          <Link
+            href="/blog"
+            className={cn(buttonVariants({ variant: "ghost" }))}
+          >
+            全ての記事を見る
+          </Link>
+        </div>
       </article>
     </>
   );
--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -12,7 +12,7 @@ export default function MainNav({ items }: MainNavProps) {
   return (
     <>
       <div className="flex items-center md:gap-10">
-        <Link href={""} className="hidden md:flex items-center space-x-2">
+        <Link href={"/"} className="hidden md:flex items-center space-x-2">
           <span className="font-bold hidden sm:inline-block">Post Writer</span>
         </Link>
         <nav className="md:flex gap-6 hidden">
