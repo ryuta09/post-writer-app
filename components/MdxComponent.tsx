9bf90b9c1651a94e2fd62896cb028067c6f7d54a
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-05-18 22:01:32 +0900

    faet: マークダウンで出力する際のエラー解消とコンポーネントの作成

--- components/MdxComponent.tsx
+++ components/MdxComponent.tsx
@@ -1,11 +1,16 @@
 'use client'
 import { useMDXComponent } from "next-contentlayer/hooks";
-
+import Image from "next/image";
+import Callout from "./callout";
+const components = {
+  Image,
+  Callout
+}
 export default function Mdx({code}: {code: string}) {
   const Component = useMDXComponent(code)
   return (
     <>
-      <Component />
+      <Component components={components}/>
     </>
   );
 }
