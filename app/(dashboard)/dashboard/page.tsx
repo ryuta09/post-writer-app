66c3b7203165ec1f466275eb5b5069b5f6ea1c27
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-05-22 21:56:51 +0900

    feat: 共通のheaderを作成

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,13 +1,12 @@
+import DashboardHeader from "@/components/DashboardHeader";
 import DashboardShell from "@/components/DashboardShell";
 
 export default function DashboardPage() {
-  return(
+  return (
     <>
-    <DashboardShell>
-      <div>
-
-      </div>
-    </DashboardShell>
+      <DashboardShell>
+        <DashboardHeader heading="記事作成" text="投稿の作成と管理">Create</DashboardHeader>
+      </DashboardShell>
     </>
-  )
+  );
 }
