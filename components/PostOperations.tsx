b7c3a3e0985c103101fb7d51be6a2ab93114388d
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-05-25 12:33:48 +0900

    feat: 削除ボタンをクリックしたときにalertが出るように実装

--- components/PostOperations.tsx
+++ components/PostOperations.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Post } from "@prisma/client";
 import {
   DropdownMenu,
@@ -6,13 +7,26 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Icons } from "./Icon";
 import Link from "next/link";
+import { useState } from "react";
 
 interface PostOperationsProps {
   post: Pick<Post, "id" | "title">;
 }
 export default function PostOperations({ post }: PostOperationsProps) {
+  const [showDeleteAlert, setShowDeleteAlert] = useState<boolean>(false)
   return (
     <>
       <DropdownMenu>
@@ -21,14 +35,31 @@ export default function PostOperations({ post }: PostOperationsProps) {
         </DropdownMenuTrigger>
         <DropdownMenuContent>
           <DropdownMenuItem>
-            <Link href={`/editor/${post.id}`} className="w-full">編集</Link>
+            <Link href={`/editor/${post.id}`} className="w-full">
+              編集
+            </Link>
           </DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem className="text-destructive cursor-pointer focus:text-destructive">
+          <DropdownMenuItem className="text-destructive cursor-pointer focus:text-destructive" onClick={() => setShowDeleteAlert(true)}>
             削除
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
+
+      <AlertDialog open={showDeleteAlert} onOpenChange={setShowDeleteAlert}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>本当にこの記事を削除しますか？</AlertDialogTitle>
+            <AlertDialogDescription>
+              この操作は取り消しができません。
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>キャンセル</AlertDialogCancel>
+            <AlertDialogAction>削除</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   );
 }
